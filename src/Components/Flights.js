449c4395330bb45182d1c945806d3f0753906dbe
rubenrudov <ruby.rudov@gmail.com>
2021-04-18 15:53:11 +0300

    API trying - first fetch with Skyscanner API

--- src/Components/Flights.js
+++ src/Components/Flights.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar'
+import axios from "axios";
 
 export default function Flights() {
+    const [flights, setFlights] = useState(["Null"]);
+
+    useEffect(() => {
+        fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "1aef007bdcmshc0731e404515fc0p1133fbjsndcab84fe524b",
+                "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+            }
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log(data.Places[0]);
+            setFlights(data.Places[0]);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }, []);
+
     return (
         <div>
             <Navbar/>
             <h1>Flights page</h1>
+            <p>{flights.PlaceId}</p>
         </div>
     );
 }
