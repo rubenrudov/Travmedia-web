d2cf9c8a88c144e15879cb6941b4d129be5c0196
rubenrudov <ruby.rudov@gmail.com>
2021-04-24 12:18:07 +0300

    User warning for flights page

--- src/Components/Flights.js
+++ src/Components/Flights.js
@@ -12,6 +12,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 
+// Warning
 const styles = (theme) => ({
     root: {
       margin: 0,
